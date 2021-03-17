9906a6c5ef8a30b3474eba03986ec6253f775202
Hutson <i@htsn.us>
2021-03-17 13:50:40 -0500

    Initial Commit

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Hutson on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
